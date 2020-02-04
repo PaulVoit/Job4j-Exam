d9f859a35f87383180cf1b19ae00623a2ebea22a
PaulVoit <paulv2007@yandex.ru>
2020-02-04 22:55:36 +0300

    1. Activity. Жизненный цикл.[#231151]

--- app/src/main/java/ru/job4j/ExamActivity.java
+++ app/src/main/java/ru/job4j/ExamActivity.java
@@ -3,12 +3,62 @@ package ru.job4j;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ExamActivity extends AppCompatActivity {
+
+    private static final String TAG = "ExamActivity";
+    private static final String KEY_NUMBER = "number";
+    private int count = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.custom_layout);
+        if (savedInstanceState != null){
+            count = savedInstanceState.getInt(KEY_NUMBER, 0);
+            count++;
+        }
+        Log.d(TAG, String.valueOf(count));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_NUMBER, count);
+        Log.d(TAG, "outState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
 }
