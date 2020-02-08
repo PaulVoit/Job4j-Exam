d45568f598eecfd8b1d3dddd38beb372f294eef1
PaulVoit <paulv2007@yandex.ru>
2020-02-08 18:56:11 +0300

    2. MVC. Создания события и получение данных от пользователя.[#231152]

--- app/src/main/java/ru/job4j/ExamActivity.java
+++ app/src/main/java/ru/job4j/ExamActivity.java
@@ -4,61 +4,126 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ru.job4j.model.Option;
+import ru.job4j.model.Question;
 
 public class ExamActivity extends AppCompatActivity {
 
     private static final String TAG = "ExamActivity";
-    private static final String KEY_NUMBER = "number";
-    private int count = 0;
+    private RadioGroup variants;
+    private Button previous;
+    private Button next;
+    private TextView text;
 
+    private int position = 0;
+    private final List<Question> questions = Arrays.asList(
+            new Question(
+                    1, "How many primitive variables does Java have?",
+                    Arrays.asList(
+                            new Option(1, "1.1"), new Option(2, "1.2"),
+                            new Option(3, "1.3"), new Option(4, "1.4")
+                    ), 4
+            ),
+            new Question(
+                    2, "What is Java Virtual Machine?",
+                    Arrays.asList(
+                            new Option(1, "2.1"), new Option(2, "2.2"),
+                            new Option(3, "2.3"), new Option(4, "2.4")
+                    ), 4
+            ),
+            new Question(
+                    3, "What is happen if we try unboxing null?",
+                    Arrays.asList(
+                            new Option(1, "3.1"), new Option(2, "3.2"),
+                            new Option(3, "3.3"), new Option(4, "3.4")
+                    ), 4
+            )
+    );
+
+    private List<Integer> answers = new ArrayList<>(questions.size());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate");
-        setContentView(R.layout.custom_layout);
-        if (savedInstanceState != null){
-            count = savedInstanceState.getInt(KEY_NUMBER, 0);
-            count++;
+        setContentView(R.layout.activity_exam);
+        variants = findViewById(R.id.variants);
+        previous = findViewById(R.id.previous);
+        next = findViewById(R.id.next);
+        text = findViewById(R.id.question);
+        variants.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (position != questions.size() - 1) {
+                    next.setEnabled(true);
+                }
+            }
+        });
+        this.fillForm();
+        next.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showAnswer();
+                        saveAnswer();
+                        position++;
+                        fillForm();
+                    }
+                }
+        );
+        previous.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        position--;
+                        fillForm();
+                    }
+                }
+        );
+
+    }
+
+
+    private void fillForm() {
+
+        Question question = this.questions.get(this.position);
+        text.setText(question.getText());
+
+        for (int index = 0; index != variants.getChildCount(); index++) {
+            RadioButton button = (RadioButton) variants.getChildAt(index);
+            Option option = question.getOptions().get(index);
+            button.setId(option.getId());
+            button.setText(option.getText());
         }
-        Log.d(TAG, String.valueOf(count));
+        variants.clearCheck();
+        previous.setEnabled(position != 0);
+        next.setEnabled(false);
+
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
+
+    private void showAnswer() {
+        int id = variants.getCheckedRadioButtonId();
+        Question question = this.questions.get(this.position);
+        Toast.makeText(
+                this, "Your answer is " + id + ", correct is " + question.getAnswer(),
+                Toast.LENGTH_SHORT
+        ).show();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putInt(KEY_NUMBER, count);
-        Log.d(TAG, "outState");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
+    private void saveAnswer() {
+        this.answers.add(position, variants.getCheckedRadioButtonId());
     }
 
 }
