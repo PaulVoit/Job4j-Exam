13c49aede9000e89305141c02c928590d774b41c
PaulVoit <paulv2007@yandex.ru>
2020-10-03 22:55:24 +0300

    02 - Main (UI) Thread, runOnUiThread[91383#231179]

--- backgroundthread/src/main/java/ru/jo4j/backgroundthread/MainActivity.java
+++ backgroundthread/src/main/java/ru/jo4j/backgroundthread/MainActivity.java
@@ -2,41 +2,36 @@ package ru.jo4j.backgroundthread;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.nfc.Tag;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Background";
     private volatile boolean stopThread = false;
+    private ImageView imageView;
+    private TestThread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
     }
 
     public void startThread(View view) {
-        stopThread = true;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int count = 0;
-                while (stopThread) {
-                    Log.d(TAG, "startThread: " + count);
-                    count++;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        thread = new TestThread(10, imageView);
+        thread.start();
     }
-
 
     public void stopThread(View view) {
         stopThread = false;
--- backgroundthread/src/main/java/ru/jo4j/backgroundthread/TestThread.java
+++ backgroundthread/src/main/java/ru/jo4j/backgroundthread/TestThread.java
@@ -1,21 +1,32 @@
 package ru.jo4j.backgroundthread;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
+import java.net.URL;
 
 
 public class TestThread extends Thread {
     private int times;
     private static final String TAG = "BackgroundThread";
+    private Handler mainHandler = new Handler();
+    private ImageView imageView;
 
-    public TestThread(int times) {
+    public TestThread(int times, ImageView imageView) {
         this.times = times;
+        this.imageView = imageView;
     }
 
     @Override
     public void run() {
         int count = 0;
+        loadImageFromNetwork(imageView);
         while (count != 10) {
             Log.d(TAG, "startThread" + count);
             count++;
@@ -25,6 +36,14 @@ public class TestThread extends Thread {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
+    private void loadImageFromNetwork(final ImageView imageView) {
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Picasso.get().load("https://uranote.ru/wp-content/uploads/2018/04/https1-e1524122703248.png").into(imageView);
+            }
+        });
     }
 }
